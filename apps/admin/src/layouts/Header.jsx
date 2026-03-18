a16ffde2183c4865ef201696288d1ad544455d45
pengpon <bd102pon@gmail.com>
2026-03-18 10:52:22 +0800

    feat(admin): add logout

--- apps/admin/src/layouts/Header.jsx
+++ apps/admin/src/layouts/Header.jsx
@@ -1,4 +1,36 @@
+import { Toast } from '@repo/utils';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router';
+import { logoutAsync } from '../store/slices/authSlice';
+
 function Header() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await dispatch(logoutAsync()).unwrap();
+      Toast.fire({
+        position: 'top',
+        icon: 'success',
+        title: 'Successfully Logged Out',
+        color: '#1f2937',
+        iconColor: '#10b981',
+        background: '#ffffff',
+      });
+      navigate('/login');
+    } catch (errorMessage) {
+      Toast.fire({
+        position: 'top',
+        icon: 'error',
+        title: errorMessage,
+        color: '#1f2937',
+        iconColor: '#ef4444',
+        background: '#ffffff',
+      });
+    }
+  };
+
   return (
     <>
       <header className="z-10 flex h-16 items-center justify-between bg-white px-8 shadow-sm">
@@ -14,6 +46,7 @@ function Header() {
             <span className="text-gray-600">Hello, Admin</span>
           </div>
           <button
+            onClick={handleLogout}
             type="button"
             className="rounded-lg bg-slate-100 px-2 py-1.5 text-sm font-medium text-slate-700 transition hover:bg-red-50 hover:text-red-600"
           >
