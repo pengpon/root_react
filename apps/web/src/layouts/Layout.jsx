c9d464dae2f285366d7df7afc1a9f509b520bf35
pengpon <bd102pon@gmail.com>
2026-03-22 23:56:47 +0800

    feat(web): implement ScrollRestoration for better navigation UX

--- apps/web/src/layouts/Layout.jsx
+++ apps/web/src/layouts/Layout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router';
+import { Outlet, ScrollRestoration } from 'react-router';
 import Footer from './Footer';
 import Header from './Header';
 
@@ -10,6 +10,7 @@ function Layout() {
         <main className="flex-1">
           <Outlet />
         </main>
+        <ScrollRestoration />
         <Footer />
       </div>
     </>
